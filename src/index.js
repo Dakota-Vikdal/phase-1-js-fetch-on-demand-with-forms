274f1d735dc093f76786c2eacbd672ec3767313f
Dakota Vikdal <dakota.james19@hotmail.com>
2023-01-22 14:42:47 -0500

    fetch lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
+
+
 const init = () => {
+  const form = document.querySelector("form");
+  form.addEventListener("submit", event => {
+    event.preventDefault()
+    
+    const input = document.querySelector("#searchByID")
+    console.log(input.value)
+    
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then(r => r.json())
+    .then(data => titleAndSummary(data))
+
+    function titleAndSummary(data) {
+      const title = document.querySelector("#movieDetails > h4")
+      const summary = document.querySelector("#movieDetails > p")
+      title.textContent = data.title
+      summary.textContent = data.summary
+      console.log(title)
+      console.log(summary)
+    }
   
-}
+
+    
+});
+};
+
+
+
 
 document.addEventListener('DOMContentLoaded', init);
+
